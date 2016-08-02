260d1dec0268d815588635fa4bc3272f65ad9e4c
mahroni <mahroni87@gmail.com>
2016-08-02 11:32:07 +0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+class App extends React.Component {
+    render() {
+        return React.createElement('h1', null, 'Hello Guys')
+    }
+}
+
+// const App = () => <h1> Hello Mahroni</h1>
+
+export default App
